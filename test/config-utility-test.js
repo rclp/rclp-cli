cd4130e807fbdab4ccc0aa69d4fd421be0d79235
Atsuya Takagi <asoftonight@gmail.com>
2020-08-02 22:14:36 +0900

    support macos and windows

--- test/config-utility-test.js
+++ test/config-utility-test.js
@@ -6,29 +6,79 @@
  */
 
 const assert = require('assert')
+const os = require('os')
+const path = require('path')
 
 const ConfigUtility = require('../lib/config-utility')
 
 describe('ConfigUtility', () => {
-  context('Config file', () => {
+  context('on Linux', () => {
+    before(function beforeFunction() {
+      if (os.type().toLowerCase() !== 'linux') {
+        this.skip()
+      }
+    })
+
     it('returns config file path for Linux', () => {
       assert.strictEqual(
           ConfigUtility.configPathLinux(),
-          `${process.env.HOME}/.config/rclp/rclp.json`)
+          path.resolve(process.env.HOME, '.config', 'rclp', 'rclp.json'))
     })
 
-    it.skip('returns config file path for Windows', () => {
-      assert.strictEqual(
-          ConfigUtility.configPathWindows(),
-          `${process.env.HOME}/.config/rclp/rclp.json`)
-    })
-  })
-
-  context('Credential', () => {
     it('returns credential file path for Linux', () => {
       assert.strictEqual(
           ConfigUtility.credentialPathLinux(),
-          `${process.env.HOME}/.config/rclp/credential.json`)
+          path.resolve(process.env.HOME, '.config', 'rclp', 'credential.json'))
+    })
+  })
+
+  context('on macOS', () => {
+    before(function beforeFunction() {
+      if (os.type().toLowerCase() !== 'darwin') {
+        this.skip()
+      }
+    })
+
+    it('returns config file path for macOS', () => {
+      assert.strictEqual(
+          ConfigUtility.configPathMacOs(),
+          path.resolve(
+              process.env.HOME,
+              'Library',
+              'Application Support',
+              'rclp',
+              'rclp.json'))
+    })
+
+    it('returns credential file path for macOS', () => {
+      assert.strictEqual(
+          ConfigUtility.credentialPathMacOs(),
+          path.resolve(
+              process.env.HOME,
+              'Library',
+              'Application Support',
+              'rclp',
+              'credential.json'))
+    })
+  })
+
+  context('on Windows', () => {
+    before(function beforeFunction() {
+      if (os.type().toLowerCase() !== 'windows_nt') {
+        this.skip()
+      }
+    })
+
+    it('returns config file path for Windows', () => {
+      assert.strictEqual(
+          ConfigUtility.configPathWindows(),
+          path.resolve(process.env.APPDATA, 'rclp', 'rclp.json'))
+    })
+
+    it('returns credential file path for Windows', () => {
+      assert.strictEqual(
+          ConfigUtility.credentialPathWindows(),
+          path.resolve(process.env.HOME, 'rclp', 'credential.json'))
     })
   })
 })
